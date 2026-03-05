0a9bdbcd8170385d743c1931056d7affa88d2629
markpelly <markpelly@users.noreply.github.com>
2026-03-05 14:04:32 -0500

    Fetch and display ingredients from Smoothiefroot API
    Added functionality to fetch and display smoothie ingredients from an external API.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -22,12 +23,13 @@ ingredients_list = st.multiselect(
     , max_selections=5
 )
 
-if ingredients_list:
-    
+if ingredients_list:    
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+        sf__df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
 
@@ -44,7 +46,4 @@ if ingredients_list:
         
         st.success('Your Smoothie is ordered!', icon="✅")
 
-        import requests  
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-        # st.text(smoothiefroot_response.json())
-        sf__df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+    
